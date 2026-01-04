c44ab2f7c3e443dd71365fbf90d92878bf3ab8b4
Quan H Le <114127739+JoshuaLe0112@users.noreply.github.com>
2026-01-04 00:39:05 -0600

    Adding Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { SignedOut, SignUpButton, SignedIn, SignOutButton } from "@clerk/nextjs";
 
 export default function Home() {
- return <Button>Click Me</Button>
+  return(
+  <div>
+    <h1>Home Page</h1>
+      
+      <SignedOut>
+        <SignUpButton mode = "modal">Sign Up</SignUpButton>
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton>Logout</SignOutButton>
+      </SignedIn>
+
+
+  </div>
+  );
 }
